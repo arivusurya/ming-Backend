12404deb6b35d914ab6b1d0d45a1215b29136712
Arivumathi R <60580194+arivusurya@users.noreply.github.com>
2023-06-25 21:30:49 +0530

    Update index.js
    index updated

--- index.js
+++ index.js
@@ -3,8 +3,19 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 
+const rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 100,
+  onLimitReached: (req, res, options) => {
+    res.status(429).send("Too many requests, please try again later.");
+  },
+});
+
 const app = express();
 
+app.use(limiter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors("*"));
@@ -12,8 +23,31 @@ app.use(morgan("dev"));
 
 app.use("/api/v1", require("./version/v1/router"));
 
+app.get("/", (req, res) => {
+  const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  return res.json({
+    message: "working!",
+    ip: ip,
+  });
+});
+
+app.use("*", (req, res) => {
+  return res.json({
+    message: "NOT_FOUND!",
+  });
+});
+
+process.on("uncaughtException", function (err) {
+  console.log(new Date().toUTCString() + " uncaughtException:", err.message);
+  // process.exit(1);
+});
+
 const PORT = process.env.PORT ?? 5000;
 
-app.listen(PORT, async () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+if (process.env.SERVERLESS !== true) {
+  app.listen(PORT, async () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
