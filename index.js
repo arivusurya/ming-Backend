08db21dded02e29030619aafbf2eef614d4b5afd
arivusurya <arivusurya.3@gmail.com>
2023-07-19 21:32:39 +0530

    remove Console on index.js

--- index.js
+++ index.js
@@ -60,7 +60,6 @@ const PORT = process.env.PORT ?? 5000;
 if (process.env.SERVERLESS !== true) {
   app.listen(PORT, async () => {
     // await OrderItem.sync({ force: true });
-    console.log("its worked");
     console.log(`Server is running on port ${PORT}`);
   });
 }
