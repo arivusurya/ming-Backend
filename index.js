d34da4a9a6f7b2676eed0de0666e599550fb0ce1
Gandhisurya <gandhisurya508@gmail.com>
2023-07-22 19:08:41 +0530

    ratelimit remove

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const {
   TokenCollector,
 } = require("./version/v1/Scheduler/ScheduleTask");
 const cron = require("node-cron");
-const rateLimit = require("express-rate-limit");
 
 const limiter = rateLimit({
   windowMs: 60 * 1000,
@@ -55,7 +54,6 @@ const PORT = process.env.PORT ?? 5000;
 
 if (process.env.SERVERLESS !== true) {
   app.listen(PORT, async () => {
-    console.log("april Fool")
     console.log(`Server is running on port ${PORT}`);
   });
 }
